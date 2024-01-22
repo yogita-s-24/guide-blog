65caef29f22369acb0e45b3b81ca3acea7209c30
Yogita Shete <yogitashete24@gmail.com>
2024-01-22 15:04:14 +0530

    feat: Implement React router in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import SignIn from "./Views/Signin";
+import Projects from "./Views/Projects";
+import Dashboard from "./Views/Dashboard";
+import SignUp from "./Views/Signup";
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-export default App
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home/>
+  },
+  {
+    path : '/about',
+    element : <About/>
+  },
+  {
+    path : '/dashboard',
+    element : <Dashboard/>
+  },
+  {
+    path : '/projects',
+    element : <Projects/>
+  },
+  {
+    path : '/sign-in',
+    element : <SignIn/>
+  },
+  {
+    path: '/sign-up',
+    element : <SignUp/>
+  }
+]);
+
+root.render(<RouterProvider router={router}/>);
+
+export default App;
