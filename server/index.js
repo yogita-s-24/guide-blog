fa063c406bf32d7571b8b6d23db16e60718337b6
Yogita Shete <yogitashete24@gmail.com>
2024-01-23 17:01:42 +0530

    create and run the server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
