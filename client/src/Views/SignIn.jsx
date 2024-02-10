9e40f83bdd26c719259c70f3e56a6be0667c1544
Yogita Shete <yogitashete24@gmail.com>
2024-02-10 10:50:56 +0530

    complete signup ui

--- client/src/Views/SignIn.jsx
+++ client/src/Views/SignIn.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../components/Header'
+import { Link } from 'react-router-dom'
 
 function SignIn() {
   return (
-    <div>Signin</div>
+    <div >
+      <Header/>
+      <div className='min-h-screen mt-20'>
+      <Link
+        to="/"
+        className=" font-bold dark:text-white text-4xl  ">
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Guide's
+        </span>{" "}
+        Blog
+      </Link>
+      </div>
+      <p>This is demop project. You can sign up with your emailand password or with google.</p>
+      </div>
   )
 }
 
