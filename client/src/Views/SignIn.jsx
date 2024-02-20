319015c33b7b9d5c1d1160e186704134041f593c
Yogita Shete <yogitashete24@gmail.com>
2024-02-20 13:23:48 +0530

    add redux toolkit

--- client/src/Views/SignIn.jsx
+++ client/src/Views/SignIn.jsx
@@ -1,14 +1,18 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-import { useState } from "react";
 import FooterCamp from "../components/Footer";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({}); 
+  const {loading , error:errorMessage} = useSelector((state) => state.user) || {};
+
+const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
@@ -16,11 +20,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields.");
+      return dispatch(signInFailure("Please fill out all fields."));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+     dispatch(signInStart());
 
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -30,16 +33,15 @@ export default function SignIn() {
 
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
-      }
-      setLoading(false);
-      
+        dispatch(signInFailure(data.message));
+      }     
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
+      
     }
   };
   return (
@@ -114,3 +116,6 @@ export default function SignIn() {
     </>
   );
 }
+
+// import Header from "../components/Header";
+// import FooterCamp from "../components/Footer";
